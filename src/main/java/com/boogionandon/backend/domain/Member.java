3c1ee2215c85310aec48981539af994e286aa7c5
Jaewon94 <wodnjs3580@gmail.com>
2024-09-22 16:07:28 +0900

    Member 엔티티 관련 수정

--- src/main/java/com/boogionandon/backend/domain/Member.java
+++ src/main/java/com/boogionandon/backend/domain/Member.java
@@ -9,13 +9,19 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 @Entity
 //@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 //@DiscriminatorColumn(name="member_type")
 @Inheritance(strategy = InheritanceType.JOINED)
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
 public abstract class Member extends BaseEntity{
 
   @Id
