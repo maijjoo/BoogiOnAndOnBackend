c6b8f76f9ac68ffb6e7a632e083fd9c9261321ad
RAJUYEOB <pag1yun99@gmail.com>
2024-10-01 19:38:12 +0900

    집하지등록서비스까지

---
+++ src/main/java/com/boogionandon/backend/dto/PickUpRequestDTO.java
@@ -0,0 +1,36 @@
+package com.boogionandon.backend.dto;
+
+import com.boogionandon.backend.domain.enums.TrashType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class PickUpRequestDTO {
+//    private Long id;            // ID
+    private String submitterUsername;   // 제출자 이름
+    private String pickUpPlace; // 집하지 명
+    private Double latitude;    // 위도
+    private Double longitude;   // 경도
+    private String mainTrashType;    //주요 쓰레기 타입
+    private Double actualCollectedVolume;   // 실제 수거량
+
+    // 등록/수정용의 진짜 파일들
+    @Builder.Default
+    private List<MultipartFile> files = new ArrayList<>();
+
+    // 조회용으로 쓸 파일의 이름만 있는 List
+    @Builder.Default
+    private List<String> uploadedFileNames = new ArrayList<>();
+
+}
---
+++ src/main/java/com/boogionandon/backend/service/PickUpService.java
@@ -0,0 +1,8 @@
+package com.boogionandon.backend.service;
+
+import com.boogionandon.backend.dto.PickUpRequestDTO;
+
+public interface PickUpService {
+
+    public void insertPickUp(PickUpRequestDTO pickUpRequestDTO);
+}
