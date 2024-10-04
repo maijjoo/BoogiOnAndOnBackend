81c6c119938e68493261bbe66cb3ce2db736e6a9
Jaewon94 <wodnjs3580@gmail.com>
2024-10-04 13:01:45 +0900

    pickUp 조회 완료 - 이미지는 추후

--- src/main/java/com/boogionandon/backend/service/PickUpService.java
+++ src/main/java/com/boogionandon/backend/service/PickUpService.java
@@ -1,8 +1,18 @@
 package com.boogionandon.backend.service;
 
+import com.boogionandon.backend.dto.PickUpListForCollectorResponseDTO;
 import com.boogionandon.backend.dto.PickUpRequestDTO;
+import java.util.List;
 
 public interface PickUpService {
 
     public void insertPickUp(PickUpRequestDTO pickUpRequestDTO);
+
+    public List<PickUpListForCollectorResponseDTO> findPickUpWithAdmin(Long adminId);
+
+    void updatePickUpStatusToAddedToRoute(Long pickUpId);
+
+    void updatePickUpStatusToCompleted(Long pickUpId);
+
+    void updatePickUpStatusFromAddedToNeeded(Long pickUpId);
 }
